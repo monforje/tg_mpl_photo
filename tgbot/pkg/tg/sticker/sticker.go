9697d00919925dd5cb5d583239edc7852dd820a9
dan <daniil234agentfinger.monforje@gmail.com>
2025-11-29 14:44:47 +1000

    feat(sticker): add sticker package with basic structure
    добавл два стикера на регестрацию

---
+++ tgbot/pkg/tg/sticker/sticker.go
@@ -0,0 +1,11 @@
+package sticker
+
+var (
+	StickerRegSuccess = "CAACAgIAAxkBAAETs1BpDFNglWNcAQKAF-sAAZU1--4Sc_8AAgEBAAJWnb0KIr6fDrjC5jQ2BA"
+	StickerRegFail    = "CAACAgIAAxkBAAET48BpFfaohvrTZruZN9L5XoKkYw2eWAAC9AADVp29ChFYsPXZ_VVJNgQ"
+)
+
+func SendSticker(stikerID string) error {
+	// Implementation for sending a sticker telebot
+	return nil
+}
